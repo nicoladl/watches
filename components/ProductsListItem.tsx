135105d3d195095ad982ea613ba00b1741ed78e7
Nicola De Lazzari <>
2023-02-05 16:17:28 +0100

    refactor: mobile views

--- components/ProductsListItem.tsx
+++ components/ProductsListItem.tsx
@@ -2,7 +2,7 @@ import styles from './ProductsListItem.module.scss'
 
 export const ProductsListItem = ({ id, name, children }) => {
     return (
-        <li className={styles.productItem}>
+        <li className={styles.productListItem}>
             <a href={`/watches/${id}`} title={name}>
                 {children}
             </a>
